1eaf6ab8e880c4e9ae61fb5f6d84af44d263edd5
Komead <dkapitanov18@gmail.com>
2025-03-20 15:54:05 +0300

    Initial commit

---
+++ src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Socket socket;
+    private DataOutputStream outputStream;
+    private DataInputStream inputStream;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            inputStream = new DataInputStream(this.socket.getInputStream());
+            outputStream = new DataOutputStream(this.socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String checkMessage() {
+        String receivedMessage = "";
+        try {
+            receivedMessage = inputStream.readUTF();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return receivedMessage;
+    }
+
+    public void finish() {
+        try {
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            outputStream.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
