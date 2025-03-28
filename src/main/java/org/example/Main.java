dd1e341e464a96de0f9b006e390894d7c38d773d
Komead <dkapitanov18@gmail.com>
2025-03-28 12:43:55 +0300

    Теперь бд создаётся при запуске сервера, если её нет

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,15 @@ import org.flywaydb.core.Flyway;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/chat";
+        String url = "jdbc:mysql://localhost:3306";
         String user = "root";
         String password = "root";
 
         // Настройка Flyway
         Flyway flyway = Flyway.configure()
                 .dataSource(url, user, password)
+                .schemas("chat")
+                .createSchemas(true)
                 .locations("classpath:db/migration")
                 .load();
 
